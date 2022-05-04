5765caa1079599e3de1aec22c3b6525d17660909
Adrián Pisabarro García <apisabarro@teknei.com>
2022-05-04 12:49:12 +0200

    Add models to SQL Lite

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Este repositorio sirve para el aprendizaje de:
 * Django
 
 El documento con el Manual de Usuario, se encuentra en este enlace: [https://labur.eus/EGZNf](https://docs.google.com/document/d/1-QjsB7f2ocSsPC19IoQjOdb2CFdaheEljGYwRvXf78s/edit?usp=sharing)
+
+## Comandos para Migrations con SQL Lite
+1. Escribir las classes en el archivo `models.py`
+2. Ejecutar el comando `python manage.py makemigrations`
+3. Ejecutar el comando `python manage.py migrate`
+4. Ejecutar el comando `python manage.py runserver`
--- departamentos/models.py
+++ departamentos/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+# Models here.
+
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=30)
+
+
+class Gasto(models.Model):
+    nombre = models.CharField(max_length=30)
+    precio = models.FloatField()
+    cantidad = models.IntegerField()
+    fecha = models.DateField()
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=60)
+    email = models.EmailField()
+    habilitado = models.BooleanField()
+
+class Nomina(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    cantidad = models.FloatField()
+    fecha = models.DateField()
+
+
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=30)
+
+class Parte(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+    observacion = models.CharField(max_length=500)
+    duracion = models.TimeField()
+    fecha = models.DateField()
+
+
+
+
+
+
