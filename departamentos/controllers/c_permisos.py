1eb1c9a62b8b058f827619a2587d7fc1454ab41b
Adrián Pisabarro García <apisabarro@teknei.com>
2022-05-16 09:18:54 +0200

    Corrección atributos de permisos

--- departamentos/controllers/c_permisos.py
+++ departamentos/controllers/c_permisos.py
@@ -1,13 +1,13 @@
 from departamentos.models import Permiso, Usuario
 
 def permisos_departamentos(id_usuario):
-    permisos = Permiso.objects.filter(user=Usuario.objects.get(id=int(id_usuario)))
+    permisos = Permiso.objects.filter(usuario=Usuario.objects.get(id=int(id_usuario)))
     permisos_departamentos = []
     for permiso in permisos:
-        if str(permiso.id_usuario) == str(id_usuario):
+        if int(permiso.usuario.id) == int(id_usuario):
             # guardo el id del departamento para saber que departamento tiene permiso ese usuario y si tiene permiso para escribir o ver
             permisos_departamentos.append({
-                "departamento": str(permiso.id_departamento),
+                "departamento": str(permiso.departamento.id),
                 "escritura": permiso.escritura,
             })
     return permisos_departamentos
