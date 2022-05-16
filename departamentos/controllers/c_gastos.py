51c283a944ae59ff59606d9de5249e3f29c828b0
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-05-16 19:23:38 +0200

    Mejorar forma de ver los gastos

--- departamentos/controllers/c_gastos.py
+++ departamentos/controllers/c_gastos.py
@@ -1,8 +1,23 @@
 from departamentos.models import Gasto
 
+
 def leer_gastos():
     gastos = Gasto.objects.all()
-    return gastos
+
+    gastos_personalizados = []
+    for gasto in gastos:
+        gastos_personalizados.append(
+            {
+                "id": gasto.id,
+                "nombre": gasto.nombre,
+                "precio": gasto.precio,
+                "cantidad": gasto.cantidad,
+                "total": float(gasto.precio * gasto.cantidad),
+                "fecha": gasto.fecha
+            }
+        )
+    return gastos_personalizados
+
 
 def anadir_gasto(request):
     nombre = request.POST["nombre"]
@@ -10,6 +25,7 @@ def anadir_gasto(request):
     cantidad = request.POST["cantidad"]
     fecha = request.POST["fecha"]
     Gasto(nombre=nombre, precio=float(precio), cantidad=int(cantidad), fecha=fecha).save()
+
 
 def eliminar_gasto(request):
     id = request.POST["id"]
